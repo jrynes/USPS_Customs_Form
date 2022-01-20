1505bc0226756e361c582305b3d6a1578b7b3f19
Jeff Rynes <jrynes3@gmail.com>
2022-01-20 20:59:07 +0100

    Added Treeview to view recipient list
    Added tree view to view the recipient list - need to add check boxes

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from selenium.common.exceptions import TimeoutException
 import csv
 # Import tkinter to help create our GUI elements for the user
 import tkinter as tk
-
+from tkinter import filedialog as fd
+from tkinter import ttk
+import os
 
 
 # Helper function to parse the zip code contained in our CSV file
@@ -28,31 +30,83 @@ def waitForPageLoad(targetDriver, targetCondition, delay):
     return
 
 # NOT CURRENTLY USED - Helper function to help select our package type
-def selectPackageType(webDriver, radioGroupName, valueToFind):
-    radioGroup = webDriver.findElements(By.name(radioGroupName))
-    #try:
-    for eachItem in radioGroup:
-        if radioGroup.get(eachItem).getAttribute("note method-note").equals(valueToFind):
-            radioGroup.get(eachItem).click()
-            return
-    #except:
-        #print("Error, value not found")
+# def selectPackageType(webDriver, radioGroupName, valueToFind):
+#     radioGroup = webDriver.findElements(By.name(radioGroupName))
+#     #try:
+#     for eachItem in radioGroup:
+#         if radioGroup.get(eachItem).getAttribute("note method-note").equals(valueToFind):
+#             radioGroup.get(eachItem).click()
+#             return
+#     #except:
+#         #print("Error, value not found")
 
 #TODO: Add some logic here to have the user select the path where the target CSV file is located
+# Use a tree view function from tkinter to achieve this
+def selectRecipients(columnNames, rowContent):
+    root = tk.Tk()
+    root.title('Select Package Recipients:')
+    root.geometry('600x200')
+
+    # define columns
+    #columnNames = ('first_name', 'last_name', 'email')
+    #Clean our column names from our CSV file. If the header contains a space, replace it with an underscore for processing
+    cleanedColumnNames = [sub.replace(" ", "_") for sub in columnNames]
+    joinedColumnNames = ",".join(cleanedColumnNames)
+
+
+
+    tree = ttk.Treeview(root, columns=cleanedColumnNames, show='headings')
+
+    # define headings
+    #tree.heading('first_name', text='First Name')
+    for originalName, cleanedName in zip(columnNames, cleanedColumnNames):
+        tree.heading(cleanedName, text=originalName)
+
+    for eachRowItem in rowContent:
+        tree.insert("",tk.END,values=eachRowItem)
+
+    tree.grid(row=0, column=0, sticky='nsew')
+
+    root.mainloop()
+
+fileTypes = [("CSV File", "*.csv")]
+
+def file_opener():
+    root = tk.Tk()
+    root.withdraw()
+    file = fd.askopenfilename(initialdir=os.getcwd(), filetypes=fileTypes, title="Choose a file.")
+    if file:
+        return file
+    else:
+        print ("Error - File not selected")
+        return False
+
 #Get our target information from our CSV file before we start
-targetCSV_Path = '/Users/Jeff/Documents/Customs_Form_v1.csv'
+targetCSV_Path = file_opener()
+#targetCSV_Path = '/Users/Jeff/Documents/Customs_Form_v1.csv'
+
+labels = []
 sendingAddress = []
 recipients = []
 
 with open(targetCSV_Path) as fd:
     reader = csv.reader(fd)
     for idx, row in enumerate(reader):
-        if idx == 1:
-            sendingAddress = row
-        elif idx >= 2:
-            recipients.append(row)
+        if idx == 0:
+            labels = row
+        elif row[0] == "Sender":
+            if len(sendingAddress) == 0:
+                sendingAddress = row
+            else:
+                print("Error - Two rows are marked as the package sender.")
+        elif row[0] == "Reciever":
+            if row not in recipients:
+                recipients.append(row)
+            else:
+                print("Error - Duplicate addresses are in the source CSV file.")
 
 # TODO: Add some logic here to present a checkbox for the user to select which people to send the package to
+checkedRecipients = selectRecipients(labels, recipients)
 
 
 for eachRow in recipients:
